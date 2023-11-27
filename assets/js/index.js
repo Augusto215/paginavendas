c683c1a6b18e37c61bec1c1d4aabc3830a5bd03e
Augusto215 <augusto.dataanalysis@gmail.com>
2023-11-27 17:26:59 -0300

    Novo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 window.onload = function() {
     // Substitui o estado atual do histórico pelo URL desejado
-    history.replaceState(null, null, 'http://127.0.0.1:5500/main.html');
+    history.replaceState(null, null, 'https://paginavendas.onrender.com/main.html');
 
     // Define o manipulador para o evento de "popstate"
     window.onpopstate = function(event) {
         // Redireciona para 'voltar.html' quando o botão voltar é pressionado
-        window.location.href = 'http://127.0.0.1:5500/main.html';
+        window.location.href = 'https://paginavendas.onrender.com/main.html';
     };
 };
